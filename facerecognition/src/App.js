608a6b37d79d22bddeeffc3b5711919537de7a74
Dima <dima@MacBook-Air-3.hitronhub.home>
2020-06-11 13:54:58 -0600

    updates and connected server side api

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -33,14 +33,25 @@ class App extends Component {
       imgUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id:'',
+        name: '',
+        email: '',
+        entries: 0,
+        joind: ''
+      }
     }
   }
 
-  componentDidMount() {
-    fetch('http://localhost:3000')
-      .then(response => response.json())
-        .then()
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joind: data.joind
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -65,11 +76,26 @@ class App extends Component {
   onInputChange = (event) => {
     this.setState({input: event.target.value })
   }
+  
   onButtonSubmit = () => {
     this.setState({imgUrl: this.state.input});
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response =>
-    this.displayFaceBox(this.calculateFaceLocation(response)))
+    .then(response => {
+    if (response) {
+      fetch('http://localhost:3000/image', {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          id: this.state.user.id
+        })
+      })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, { entries: count}))
+        })
+    }
+    this.displayFaceBox(this.calculateFaceLocation(response))
+    })
     .catch(err => console.log(err));
   }
 
@@ -93,7 +119,7 @@ class App extends Component {
       { route === 'home'
       ? <div>
       <Logo/>
-      <Rank/>
+      <Rank name={this.state.user.name} entries={this.state.user.entries}/>
       <ImageLinkForm 
         onInputChange={this.onInputChange} 
         onButtonSubmit={this.onButtonSubmit}/>
@@ -101,8 +127,8 @@ class App extends Component {
       </div>
       : 
       ( route === 'signin'
-      ?  <Singin onRouteChange={this.onRouteChange}/>
-      :  <Register onRouteChange={this.onRouteChange}/>
+      ?  <Singin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+      :  <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
       )
 
       
